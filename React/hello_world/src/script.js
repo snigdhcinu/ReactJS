dfca49fbc2c35dab1937b042be2c8d2d53da05b0
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-06-24 11:01:47 +0530

    my hello world project

---
+++ React/hello_world/src/script.js
@@ -0,0 +1,12 @@
+const React=require('react');
+const ReactDOM=require('react-dom');
+ReactDOM.render(
+  <div>
+    <p>Hello world</p>
+    <ul>
+        <li>This is first program</li>
+        <li>It uses JSX</li>
+        <li>I have made this on Atom</li>
+    </ul>
+  </div>
+)
